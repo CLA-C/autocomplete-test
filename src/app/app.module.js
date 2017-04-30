2fefdad9bb2c39b0b10b07da34d4ea7fd16e963e
Claudio Cordoni <Claudio Cordoni>
2017-04-30 09:27:00 +0200

    Created base angularjs app + webpack

---
+++ src/app/app.module.js
@@ -0,0 +1,11 @@
+import angular from 'angular';
+import { AppComponent } from './app.component';
+
+import '../style/app.css';
+
+export const AppModule = angular
+  .module('app', [
+      
+  ])
+  .component('app', AppComponent)
+  .name;
